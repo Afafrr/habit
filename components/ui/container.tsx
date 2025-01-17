6710db60f9f420c8af47341abc2db136c1a9031c
Afafrr <mateusz.aftanski@gmail.com>
2025-01-17 12:01:19 +0100

    feat: global container created and default button and input height changed

---
+++ components/ui/container.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+// component which positions content with proper padding, margin and media query
+export default function Container({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`max-w-screen-2xl mx-auto px-2 sm:px-4 md:px-5 xl:px-8    ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
